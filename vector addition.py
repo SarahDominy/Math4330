3a88e312d499460cc122ae3c4f6d808ae62e0dd5
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-03-07 13:39:22 -0600

    Update vector addition.py

--- vector addition.py
+++ vector addition.py
@@ -1,19 +1,14 @@
-#write a function for vector addition
-#1. 3 questions
-#2. write algorithm
-#3. implement your algorithm
-    #1) what do we want to do?
-    # we want to add two vectors
-    #2) what do we have?
-    # we have two vectors
-    #3) what do we want to have?
-    # we want a vector that is the sum of the vectors
-a = [1, 2, 3]
-b = [3, 4, 5]
 def vectorAddition(vectora,vectorb):
+    """
+    This algorithm calculates the sum of two vectors.
+
+    Args:
+        Two vectors, each representing a list of numbers
+    Returns:
+        A vector that is the sum of both vectors.
+    """
     resultant = a
     for i in range (0,len(vectora)):
         resultant[i] = vectora[i] + vectorb[i]
-    print ("resultant = ", end = "")
-    print (resultant)
-vectorAddition(a,b)
+    return resultant      
+        
