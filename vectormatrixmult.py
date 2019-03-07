d530dbdb13bfbc84bef581f5507f68f2e8aa919b
Sarah Dominy <40004337+SarahDominy@users.noreply.github.com>
2019-03-07 13:37:10 -0600

    Update vectormatrixmult.py

--- vectormatrixmult.py
+++ vectormatrixmult.py
@@ -1,4 +1,12 @@
 def matrixvector(matrixa,vectorx):
+    """
+    Multiplies a matrix by a vector.
+    
+    Args:
+        A matrix representing a list of vectors and a vector representing a list of numbers.
+    Returns:
+        A matrix where all of the vectors have been multiplied by the vector input.
+    """
     result = [0] * len(matrixa[0])
     for i in range (len(vectorx)):
         result = vectoradd(result,scalarvectormult(vectorx[i],matrixa[i]))
